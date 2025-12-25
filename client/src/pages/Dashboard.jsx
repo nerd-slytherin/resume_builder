9a9a5a97a4caac0d618e6daec24994edc6e2f79c
Tanay Pandey <tanaypandey004@gmail.com>
2025-12-25 19:57:08 +0530

    Frontend under progress : Created Login Page and Updated Layout to include Navbar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,22 @@
+import { PlusIcon, UploadCloudIcon } from 'lucide-react'
 import React from 'react'
 
 const Dashboard = () => {
   return (
     <div>
-      <h1>Dashboard page</h1>
+      <div className='max-w-7xl mx-auto px-4 py-8'>
+        <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate700 bg-clip-text text-transparent sm:hidden'>Welcome, Kashish Tiwari</p>
+        <div className='flex gap-4'>
+          <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+            <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-600 text-white rounded-full'/>
+            <p className='text-sm group-hover:text-indigo-600 transition-all duration-300'>Create Resume</p>
+          </button>
+          <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+            <UploadCloudIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-purple-300 to-purple-600 text-white rounded-full'/>
+            <p className='text-sm group-hover:text-purple-600 transition-all duration-300'>Upload Existing</p>
+          </button>
+        </div>  
+      </div>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Banner from '../components/Home/Banner'
 import Hero from '../components/Home/Hero'
 import Features from '../components/Home/Features'
 import Testimonial from '../components/Home/Testimonial'
+import CallToAction from '../components/Home/CallToAction'
+import Footer from '../components/Home/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Hero />
       <Features />
       <Testimonial />
+      <CallToAction />
+      <Footer />
     </div>
   )
 }
